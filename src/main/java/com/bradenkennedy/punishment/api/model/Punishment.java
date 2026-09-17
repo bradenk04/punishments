c385cceb124cb6c69c7744b1b7decf90fac7c6b9
Braden <bradenk04@proton.me>
2026-09-17 10:51:01 -0700

    feat: Added revoked field to punishment record

--- src/main/java/com/bradenkennedy/punishment/api/model/Punishment.java
+++ src/main/java/com/bradenkennedy/punishment/api/model/Punishment.java
@@ -11,7 +11,7 @@ public record Punishment(
         @NotNull PunishmentType type,
         @NotNull PunishmentIssuer issuer,
         @Nullable String reason,
-        @Nullable Instant expiry
-) {
+        @Nullable Instant expiry,
+        @NotNull boolean revoked) {
 
 }
